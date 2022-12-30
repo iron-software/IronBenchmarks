6792e06b98c5fc1c745dc1b523fd8ab23aa98aea
Artem Koloskov <artem@ironsoftware.com>
2022-12-30 16:36:43 +0700

    Add console arguments to run excel, pdf or barcode benchmarks from console.
    also refacrot IronBenchmarks.App.Program to be more readable.

--- IronBenchmarks.App/Program.cs
+++ IronBenchmarks.App/Program.cs
@@ -13,79 +13,137 @@ using IronBenchmarks.Reporting.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Globalization;
 using System.Reflection;
-using static Org.BouncyCastle.Math.EC.ECCurve;
 
-Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+var host = SetupApplication();
 
-var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
-var builder = new ConfigurationBuilder()
-    .AddJsonFile($"appsettings.json", true, true)
-    .AddJsonFile($"appsettings.{environment}.json", true, true)
-    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
-    .AddEnvironmentVariables();
+ApplyIronXLLicenseKey(host);
 
-var configurationRoot = builder.Build();
-
-var host = Host.CreateDefaultBuilder()
-    .ConfigureServices((context, services) =>
-    {
-        services.AddSingleton<IAppConfig, AppConfig>(
-            _ => configurationRoot.GetSection(nameof(AppConfig)).Get<AppConfig>() ?? new AppConfig());
-        services.AddSingleton<IReportingConfig, ReportingConfig>(
-            _ => configurationRoot.GetSection(nameof(ReportingConfig)).Get<ReportingConfig>() ?? new ReportingConfig());
-    })
-    .Build();
-
-var appConfig = ActivatorUtilities.GetServiceOrCreateInstance<IAppConfig>(host.Services);
-IronXL.License.LicenseKey = appConfig.LicenseKeyIronXl;
-
-var reportConfig = ActivatorUtilities.GetServiceOrCreateInstance<IReportingConfig>(host.Services);
-
-if (args.Contains("-a") || args.Contains("-append"))
-{
-    reportConfig.AppendToLastReport = true;
-}
-
+var reportConfig = GetReportConfig(args, host);
 var reportGenerator = new ReportGenerator(reportConfig);
 
-/*var excelSummaries = new List<Summary>
-{
-    BenchmarkRunner.Run<RandomCellsBenchmark>(),
-    BenchmarkRunner.Run<DateCellBenchmark>(),
-    BenchmarkRunner.Run<StyleChangeBenchmark>(),
-    BenchmarkRunner.Run<FormulaCellBenchmark>(),
-    BenchmarkRunner.Run<LoadLargeFileBenchmark>(),
-    BenchmarkRunner.Run<SaveLargeFileBenchmark>(),
-    BenchmarkRunner.Run<SortRangeBenchmark>(),
-    BenchmarkRunner.Run<AccessingRangePropertiesBenchmark>(),
-
-    //BenchmarkRunner.Run<EmptyBenchmark>(),
-    //BenchmarkRunner.Run<OtherEmptyBenchmark>(),
-};
-
-reportGenerator.GenerateReport(excelSummaries, "IronXL");
-
-/*var config = new ManualConfig()
-        .WithOptions(ConfigOptions.DisableOptimizationsValidator)
-        .AddValidator(JitOptimizationsValidator.DontFailOnError)
-        .AddLogger(ConsoleLogger.Default)
-        .AddColumnProvider(DefaultColumnProviders.Instance);
-
-var pdfSummaries = new List<Summary>
-{
-    BenchmarkRunner.Run<RenderHtmlToPdfBenchmark>(config),
-    BenchmarkRunner.Run<LoadingLargeFileBenchmark>(config),
-    BenchmarkRunner.Run<SavingLargeFileBenchmark>(config),
-};
-
-reportGenerator.GenerateReport(pdfSummaries, "IronPdf");*/
-
-var barcodeSummaries = new List<Summary>
-{
-    BenchmarkRunner.Run<CreateBarcodeBenchmark>()
-};
-
-reportGenerator.GenerateReport(barcodeSummaries, "IronBarCode");
+RunExcelBenchmarks(args, reportConfig, reportGenerator);
+RunPdfBenchmarks(args, reportConfig, reportGenerator);
+RunBarCodeBenchmarks(args, reportConfig, reportGenerator);
 
 Console.ReadKey();
+
+//#############################################################################
+
+static void RunBarCodeBenchmarks(
+    string[] args,
+    IReportingConfig reportConfig,
+    ReportGenerator reportGenerator)
+{
+    if (args.Contains("-bc"))
+    {
+        reportConfig.ReportsFolder = "Reports\\BarCode";
+
+        var barcodeSummaries = new List<Summary>
+        {
+            BenchmarkRunner.Run<CreateBarcodeBenchmark>()
+        };
+
+        reportGenerator.GenerateReport(barcodeSummaries, "BarCodeLibs");
+    }
+}
+
+static void RunPdfBenchmarks(
+    string[] args,
+    IReportingConfig reportConfig,
+    ReportGenerator reportGenerator)
+{
+    if (args.Contains("-pdf"))
+    {
+        reportConfig.ReportsFolder = "Reports\\PDF";
+
+        var config = new ManualConfig()
+            .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+            .AddValidator(JitOptimizationsValidator.DontFailOnError)
+            .AddLogger(ConsoleLogger.Default)
+            .AddColumnProvider(DefaultColumnProviders.Instance);
+
+        var pdfSummaries = new List<Summary>
+        {
+            BenchmarkRunner.Run<RenderHtmlToPdfBenchmark>(config),
+            BenchmarkRunner.Run<LoadingLargeFileBenchmark>(config),
+            BenchmarkRunner.Run<SavingLargeFileBenchmark>(config),
+        };
+
+        reportGenerator.GenerateReport(pdfSummaries, "PDFLibs");
+    }
+}
+
+static void RunExcelBenchmarks(
+    string[] args,
+    IReportingConfig reportConfig,
+    ReportGenerator reportGenerator)
+{
+    if (args.Contains("-xl"))
+    {
+        reportConfig.ReportsFolder = "Reports\\Excel";
+
+        var excelSummaries = new List<Summary>
+        {
+            BenchmarkRunner.Run<RandomCellsBenchmark>(),
+            BenchmarkRunner.Run<DateCellBenchmark>(),
+            BenchmarkRunner.Run<StyleChangeBenchmark>(),
+            BenchmarkRunner.Run<FormulaCellBenchmark>(),
+            BenchmarkRunner.Run<LoadLargeFileBenchmark>(),
+            BenchmarkRunner.Run<SaveLargeFileBenchmark>(),
+            BenchmarkRunner.Run<SortRangeBenchmark>(),
+            BenchmarkRunner.Run<AccessingRangePropertiesBenchmark>(),
+        };
+
+        reportGenerator.GenerateReport(excelSummaries, "ExcelLibs");
+    }
+}
+
+static void ApplyIronXLLicenseKey(IHost host)
+{
+    var appConfig = ActivatorUtilities
+        .GetServiceOrCreateInstance<IAppConfig>(host.Services);
+
+    IronXL.License.LicenseKey = appConfig.LicenseKeyIronXl;
+}
+
+static IReportingConfig GetReportConfig(string[] args, IHost host)
+{
+    var reportConfig = ActivatorUtilities
+        .GetServiceOrCreateInstance<IReportingConfig>(host.Services);
+
+    if (args.Contains("-a") || args.Contains("-append"))
+    {
+        reportConfig.AppendToLastReport = true;
+    }
+
+    return reportConfig;
+}
+
+static IHost SetupApplication()
+{
+    Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+    var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+    var builder = new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.json", true, true)
+        .AddJsonFile($"appsettings.{environment}.json", true, true)
+        .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+        .AddEnvironmentVariables();
+
+    var configurationRoot = builder.Build();
+
+    var host = Host.CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<IAppConfig, AppConfig>(
+                _ => configurationRoot.GetSection(nameof(AppConfig))
+                .Get<AppConfig>() ?? new AppConfig());
+            services.AddSingleton<IReportingConfig, ReportingConfig>(
+                _ => configurationRoot.GetSection(nameof(ReportingConfig))
+                .Get<ReportingConfig>() ?? new ReportingConfig());
+        })
+        .Build();
+    return host;
+}
